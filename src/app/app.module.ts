11a6f649dd132e00b7c1d77e7957b799c9f7bd7d
Cullen Kirkpatrick <cullen@540.co>
2019-04-08 20:34:14 -0400

    Adding home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { LevelsComponent } from './home/levels/levels.component';
+import { ActivityComponent } from './home/activity/activity.component';
+import { ChartsComponent } from './home/charts/charts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AdminComponent
+    AdminComponent,
+    LevelsComponent,
+    ActivityComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
